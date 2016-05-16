5ed31da345d8f41efbf7c7070f388662dd4f6d27
Fernando Oliveira <nandooliveira.al@gmail.com>
2016-05-15 23:52:28 -0300

    Testing other routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -16,9 +16,23 @@ class TextRoutes(unittest.TestCase):
 
     def test_home(self):
         """Test if the main route loads."""
-        rv = requests.get('http://127.0.0.1:5000/api/v1')
+        rv = requests.get('http://127.0.0.1:5000/api/v1/exchange_rate')
         assert rv.status_code == 200
         assert is_json(rv.text)
+
+    def test_current(self):
+        """Test if current rates loads."""
+        rv = requests.get('http://127.0.0.1:5000/api/v1/exchange_rate/current')
+        assert rv.status_code == 200
+        assert is_json(rv.text)
+
+    def test_not_found(self):
+        """Test if API returns 404 on page nor found."""
+        rv = requests.get('http://127.0.0.1:5000/not_existing_route')
+        assert rv.status_code == 404
+        assert is_json(rv.text)
+        data = json.loads(rv.text)
+        assert data['error'] == "Not found"
 
 
 def is_json(m_json):
